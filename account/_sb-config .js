3a07b1c8b101b5874cb4836493edb1c764a76d30
KUAN-STORY <384246452@qq.com>
2025-10-11 23:46:30 +0800

    Add files via upload

---
+++ account/_sb-config .js
@@ -0,0 +1,6 @@
+// /english-videos/account/_sb-config.js
+// 你的 Supabase 專案設定（公開 anon key 是安全的，請勿放 service_role）
+window.SB = {
+  url: "https://qtgwedankftrqjmzuset.supabase.co",
+  anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0Z3dlZGFua2Z0cnFqbXp1c2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDYxMDMsImV4cCI6MjA3NDQyMjEwM30.jyETpt09pgm66aCZheMgsjtbKlVmYo-lt-hrrt6BF8g"
+};
