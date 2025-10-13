a1bf1835560dabf8342ef8624b4c80c37874d04d
KUAN-STORY <384246452@qq.com>
2025-10-13 13:59:28 +0800

    Update supa.js

--- supa.js
+++ supa.js
@@ -1,9 +1,10 @@
 // supa.js（ESM 版本）
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const SUPABASE_URL = 'https://qtgwedankftrqjmzuset.supabase.co'
-const SUPABASE_ANON_KEY = '你的 key'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0Z3dlZGFua2Z0cnFqbXp1c2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDYxMDMsImV4cCI6MjA3NDQyMjEwM30.jyETpt09pgm66aCZheMgsjtbKlVmYo-lt-hrrt6BF8g';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 })();
+
 
 
 
